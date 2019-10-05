97f642b15bb6d6e9e12049b5c0092d25e77c7245
Andrew Countryman <apcountryman@gmail.com>
2019-10-04 23:16:09 -0700

    Add compilation and linking tool locating
    Supports #1.

--- toolchain.cmake
+++ toolchain.cmake
@@ -17,5 +17,64 @@
 
 cmake_minimum_required( VERSION 3.13.4 )
 
+mark_as_advanced(
+    CMAKE_TOOLCHAIN_FILE
+    CMAKE_INSTALL_PREFIX
+)
+
 set( CMAKE_SYSTEM_NAME      "Generic"   )
 set( CMAKE_SYSTEM_PROCESSOR "Atmel-AVR" )
+
+find_program( CMAKE_C_COMPILER avr-gcc )
+mark_as_advanced( CMAKE_C_COMPILER )
+if( CMAKE_C_COMPILER STREQUAL "CMAKE_C_COMPILER-NOTFOUND" )
+    message( FATAL_ERROR "avr-gcc not found" )
+endif( CMAKE_C_COMPILER STREQUAL "CMAKE_C_COMPILER-NOTFOUND" )
+
+find_program( CMAKE_CXX_COMPILER avr-g++ )
+mark_as_advanced( CMAKE_CXX_COMPILER )
+if( CMAKE_CXX_COMPILER STREQUAL "CMAKE_CXX_COMPILER-NOTFOUND" )
+    message( FATAL_ERROR "avr-g++ not found" )
+endif( CMAKE_CXX_COMPILER STREQUAL "CMAKE_CXX_COMPILER-NOTFOUND" )
+
+find_program( CMAKE_LINKER avr-ld )
+mark_as_advanced( CMAKE_LINKER )
+if( CMAKE_LINKER STREQUAL "CMAKE_LINKER-NOTFOUND" )
+    message( FATAL_ERROR "avr-ld not found" )
+endif( CMAKE_LINKER STREQUAL "CMAKE_LINKER-NOTFOUND" )
+
+find_program( CMAKE_NM avr-nm )
+mark_as_advanced( CMAKE_NM )
+if( CMAKE_NM STREQUAL "CMAKE_NM-NOTFOUND" )
+    message( FATAL_ERROR "avr-nm not found" )
+endif( CMAKE_NM STREQUAL "CMAKE_NM-NOTFOUND" )
+
+find_program( CMAKE_OBJCOPY avr-objcopy )
+mark_as_advanced( CMAKE_OBJCOPY )
+if( CMAKE_OBJCOPY STREQUAL "CMAKE_OBJCOPY-NOTFOUND" )
+    message( FATAL_ERROR "avr-objcopy not found" )
+endif( CMAKE_OBJCOPY STREQUAL "CMAKE_OBJCOPY-NOTFOUND" )
+
+find_program( CMAKE_OBJDUMP avr-objdump )
+mark_as_advanced( CMAKE_OBJDUMP )
+if( CMAKE_OBJDUMP STREQUAL "CMAKE_OBJDUMP-NOTFOUND" )
+    message( FATAL_ERROR "avr-objdump not found" )
+endif( CMAKE_OBJDUMP STREQUAL "CMAKE_OBJDUMP-NOTFOUND" )
+
+find_program( CMAKE_AR avr-ar )
+mark_as_advanced( CMAKE_AR )
+if( CMAKE_AR STREQUAL "CMAKE_AR-NOTFOUND" )
+    message( FATAL_ERROR "avr-ar not found" )
+endif( CMAKE_AR STREQUAL "CMAKE_AR-NOTFOUND" )
+
+find_program( CMAKE_RANLIB avr-ranlib )
+mark_as_advanced( CMAKE_RANLIB )
+if( CMAKE_RANLIB STREQUAL "CMAKE_RANLIB-NOTFOUND" )
+    message( FATAL_ERROR "avr-ranlib not found" )
+endif( CMAKE_RANLIB STREQUAL "CMAKE_RANLIB-NOTFOUND" )
+
+find_program( CMAKE_STRIP avr-strip )
+mark_as_advanced( CMAKE_STRIP )
+if( CMAKE_STRIP STREQUAL "CMAKE_STRIP-NOTFOUND" )
+    message( FATAL_ERROR "avr-strip not found" )
+endif( CMAKE_STRIP STREQUAL "CMAKE_STRIP-NOTFOUND" )
