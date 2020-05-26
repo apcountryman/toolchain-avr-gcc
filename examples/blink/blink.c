b58b8ac1ff49dee4bbe472bb273cc2d1d88215e5
Andrew Countryman <apcountryman@gmail.com>
2020-05-25 18:09:32 -0700

    Update copyright dates

--- examples/blink/blink.c
+++ examples/blink/blink.c
@@ -1,7 +1,7 @@
 /**
  * toolchain-avr-gcc
  *
- * Copyright 2019 Andrew Countryman <apcountryman@gmail.com>
+ * Copyright 2019, 2020 Andrew Countryman <apcountryman@gmail.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
  * file except in compliance with the License. You may obtain a copy of the License at
